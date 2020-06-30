cbe7ae5d8ce31fde3208a4a1cbc01698f8d843a1
rggs <rgswope@gmail.com>
2020-06-30 11:34:48 -0400

    Update render.py

--- code/utils/cluster/render.py
+++ code/utils/cluster/render.py
@@ -53,109 +53,114 @@ def save_progress(config, net, mapping_assignment_dataloader,
     reordered_soft_preds[:, GT_TO_ORDER[target_i]] += \
       soft_preds[:, pred_i]  # 1-1 for IIC
   reordered_soft_preds = reordered_soft_preds.cpu().numpy()
-
-  # render point cloud in GT order ---------------------------------------------
-  hues = torch.linspace(0.0, 1.0, config.gt_k + 1)[0:-1]  # ignore last one
-  best_colours = [list((np.array(hsv_to_rgb(hue, 0.8, 0.8)) * 255.).astype(
-    np.uint8)) for hue in hues]
-
-  all_colours = [best_colours]
-
-  for colour_i, colours in enumerate(all_colours):
-    scale = 50  # [-1, 1] -> [-scale, scale]
-    border = 24  # averages are in the borders
-    point_half_side = 1  # size 2 * pixel_half_side + 1
-
-    half_border = int(border * 0.5)
-
-    average_half_side = int(half_border * np.cos(np.radians(45)))
-    average_side = average_half_side * 2
-
-    image = np.ones((2 * (scale + border), 2 * (scale + border), 3),
-                    dtype=np.uint8) * 255
-
-    # image = np.zeros((2 * (scale + border), 2 * (scale + border), 3),
-    #                dtype=np.int32)
+  
+  if config.dataset == "WAVELET":
+    np.save("best_preds_arr", reordered_soft_preds)
 
     
-    #Plotting all samples; change here for dd routine; rather than iterating through all samples, we can instead simply feed the probability
-    #matrix through UMAP, and/or cluster and plot
-    for i in range(num_samples):
-      # in range [-1, 1] -> [0, 2 * scale] -> [border, 2 * scale + border]
-      coord = get_coord(reordered_soft_preds[i, :], num_classes=config.gt_k)
-      coord = (coord * scale + scale).astype(np.int32)
-      coord += border
-      pt_start = coord - point_half_side
-      pt_end = coord + point_half_side
+  else:
+    # render point cloud in GT order ---------------------------------------------
+    hues = torch.linspace(0.0, 1.0, config.gt_k + 1)[0:-1]  # ignore last one
+    best_colours = [list((np.array(hsv_to_rgb(hue, 0.8, 0.8)) * 255.).astype(
+      np.uint8)) for hue in hues]
 
-      render_c = GT_TO_ORDER[flat_targets_all[i]]
-      colour = (np.array(colours[render_c])).astype(np.uint8)
-      image[pt_start[0]:pt_end[0], pt_start[1]:pt_end[1], :] = np.reshape(
-        colour, (1, 1, 3))
+    all_colours = [best_colours]
 
-    # add on the average image per cluster in the border
-    # -------------------------
-    # dataloaders not shuffled, or jittered here
-    # either get rid of this for custom dataset, or modify? Probably get rid of...
-    averaged_imgs = [np.zeros((config.input_sz, config.input_sz, 1)) for _ in
-                     range(config.gt_k)]
-    averaged_imgs_norm = [0. for _ in range(config.gt_k)]
-    counter = 0
-    for b_i, batch in enumerate(mapping_test_dataloader):
-      imgs = batch[0].numpy()  # n, c, h, w
-      n, c, h, w = imgs.shape
-      assert (c == 1)
+    for colour_i, colours in enumerate(all_colours):
+      scale = 50  # [-1, 1] -> [-scale, scale]
+      border = 24  # averages are in the borders
+      point_half_side = 1  # size 2 * pixel_half_side + 1
 
-      for offset in range(n):
-        img_i = counter + offset
-        img = imgs[offset]
-        img = img.transpose((1, 2, 0))
-        img = img * 255
+      half_border = int(border * 0.5)
 
-        # already in right order
-        predicted_cluster_render = np.argmax(reordered_soft_preds[img_i, :])
-        predicted_cluster_gt_weight = reordered_soft_preds[
-          img_i, predicted_cluster_render]
+      average_half_side = int(half_border * np.cos(np.radians(45)))
+      average_side = average_half_side * 2
 
-        averaged_imgs[predicted_cluster_render] += \
-          predicted_cluster_gt_weight * img
-        averaged_imgs_norm[predicted_cluster_render] += \
-          predicted_cluster_gt_weight
+      image = np.ones((2 * (scale + border), 2 * (scale + border), 3),
+                      dtype=np.uint8) * 255
 
-      counter += n
+      # image = np.zeros((2 * (scale + border), 2 * (scale + border), 3),
+      #                dtype=np.int32)
 
-    for c in range(config.gt_k):
-      if averaged_imgs_norm[c] > (sys.float_info.epsilon):
-        averaged_imgs[c] /= averaged_imgs_norm[c]
 
-      averaged_img = averaged_imgs[c].astype(np.uint8)
-      averaged_img = averaged_img.repeat(3, axis=2)
-      averaged_img = Image.fromarray(averaged_img)
-      averaged_img = averaged_img.resize((average_side, average_side),
-                                         Image.BILINEAR)
-      averaged_img = np.array(averaged_img)
+      #Plotting all samples; change here for dd routine; rather than iterating through all samples, we can instead simply feed the probability
+      #matrix through UMAP, and/or cluster and plot
+      for i in range(num_samples):
+        # in range [-1, 1] -> [0, 2 * scale] -> [border, 2 * scale + border]
+        coord = get_coord(reordered_soft_preds[i, :], num_classes=config.gt_k)
+        coord = (coord * scale + scale).astype(np.int32)
+        coord += border
+        pt_start = coord - point_half_side
+        pt_end = coord + point_half_side
 
-      coord = np.zeros(config.gt_k)
-      coord[c] = 1.
-      coord = get_coord(coord, num_classes=config.gt_k)
+        render_c = GT_TO_ORDER[flat_targets_all[i]]
+        colour = (np.array(colours[render_c])).astype(np.uint8)
+        image[pt_start[0]:pt_end[0], pt_start[1]:pt_end[1], :] = np.reshape(
+          colour, (1, 1, 3))
 
-      # recall coord is for center of image
-      # [-1, 1] -> [0, 2 * (scale + half_border)]
-      coord = (coord * (scale + half_border) + (scale + half_border)).astype(
-        np.int32)
-      # -> [half_border, 2 * (scale + half_border) + half_border]
-      coord += half_border
+      # add on the average image per cluster in the border
+      # -------------------------
+      # dataloaders not shuffled, or jittered here
+      # either get rid of this for custom dataset, or modify? Probably get rid of...
+      averaged_imgs = [np.zeros((config.input_sz, config.input_sz, 1)) for _ in
+                       range(config.gt_k)]
+      averaged_imgs_norm = [0. for _ in range(config.gt_k)]
+      counter = 0
+      for b_i, batch in enumerate(mapping_test_dataloader):
+        imgs = batch[0].numpy()  # n, c, h, w
+        n, c, h, w = imgs.shape
+        assert (c == 1)
 
-      pt_start = coord - average_half_side
-      pt_end = coord + average_half_side  # exclusive
+        for offset in range(n):
+          img_i = counter + offset
+          img = imgs[offset]
+          img = img.transpose((1, 2, 0))
+          img = img * 255
 
-      image[pt_start[0]:pt_end[0], pt_start[1]:pt_end[1], :] = averaged_img
+          # already in right order
+          predicted_cluster_render = np.argmax(reordered_soft_preds[img_i, :])
+          predicted_cluster_gt_weight = reordered_soft_preds[
+            img_i, predicted_cluster_render]
 
-    # save to out_dir ---------------------------
-    img = Image.fromarray(image)
-    img.save(os.path.join(prog_out_dir,
-                          "%d_run_%d_colour_%d_pointcloud_%s.png" %
-                          (config.model_ind, render_count, colour_i, index)))
+          averaged_imgs[predicted_cluster_render] += \
+            predicted_cluster_gt_weight * img
+          averaged_imgs_norm[predicted_cluster_render] += \
+            predicted_cluster_gt_weight
+
+        counter += n
+
+      for c in range(config.gt_k):
+        if averaged_imgs_norm[c] > (sys.float_info.epsilon):
+          averaged_imgs[c] /= averaged_imgs_norm[c]
+
+        averaged_img = averaged_imgs[c].astype(np.uint8)
+        averaged_img = averaged_img.repeat(3, axis=2)
+        averaged_img = Image.fromarray(averaged_img)
+        averaged_img = averaged_img.resize((average_side, average_side),
+                                           Image.BILINEAR)
+        averaged_img = np.array(averaged_img)
+
+        coord = np.zeros(config.gt_k)
+        coord[c] = 1.
+        coord = get_coord(coord, num_classes=config.gt_k)
+
+        # recall coord is for center of image
+        # [-1, 1] -> [0, 2 * (scale + half_border)]
+        coord = (coord * (scale + half_border) + (scale + half_border)).astype(
+          np.int32)
+        # -> [half_border, 2 * (scale + half_border) + half_border]
+        coord += half_border
+
+        pt_start = coord - average_half_side
+        pt_end = coord + average_half_side  # exclusive
+
+        image[pt_start[0]:pt_end[0], pt_start[1]:pt_end[1], :] = averaged_img
+
+      # save to out_dir ---------------------------
+      img = Image.fromarray(image)
+      img.save(os.path.join(prog_out_dir,
+                            "%d_run_%d_colour_%d_pointcloud_%s.png" %
+                            (config.model_ind, render_count, colour_i, index)))
 
 
 def get_coord(probs, num_classes):
